adae99b94c0b58e763a0ccda495a20a205008e76
Shiokaa <tom.amaru09@gmail.com>
2026-03-16 08:22:42 +0000

    feat: add locals variables

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,12 +2,24 @@
 # Terraform — Outputs
 # ──────────────────────────────────────────────
 
+locals {
+  all_vm_names = concat(
+    module.test_vm.vm_names,
+    module.test_vm_2.vm_names,
+  )
+
+  all_vm_ips = concat(
+    module.test_vm.vm_ips,
+    module.test_vm_2.vm_ips,
+  )
+}
+
 output "vm_ips" {
-  value       = module.test_vm.vm_ips
+  value       = local.all_vm_ips
   description = "Adresses IP des VMs déployés"
 }
 
 output "vm_names" {
-  value       = module.test_vm.vm_names
+  value       = local.all_vm_names
   description = "Nom des VMs déployés"
 }
