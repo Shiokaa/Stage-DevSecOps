db44778bf9834346673e9c532c74ddc6408db776
Shiokaa <tom.amaru09@gmail.com>
2026-04-16 09:26:30 +0000

    feat: refactor VM outputs to consolidate names and IDs

--- terraform/modules/vm/outputs.tf
+++ terraform/modules/vm/outputs.tf
@@ -16,8 +16,3 @@ output "vm_ips" {
   value       = proxmox_virtual_environment_vm.vm[*].ipv4_addresses
   description = "Liste des adresses IP des VM (nécessite qemu-guest-agent)"
 }
-
-# modules/vm/outputs.tf
-output "debug_ci_ssh_keys" {
-  value = join("\n      - ", [for k in var.ci_ssh_keys : trimspace(startswith(k, "~/") ? file(pathexpand(k)) : k)])
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,33 +4,31 @@
 
 locals {
   all_vm_names = concat(
-    /*     module.database.vm_names,
-    module.backup.vm_names,
-    module.database-dev.vm_names,
-    module.server-web-dev.vm_names,
-    module.ci-cd.vm_names,
-    module.reverse-proxy.vm_names,
-    module.server-web.vm_names,
-    module.dns.vm_names, */
-    module.bastion.vm_names,
-    /*     module.grafana.vm_names,
-    module.loki.vm_names,
-    module.prometheus.vm_names */
+    module.bastion.vm_ids,
+    module.db.vm_ids,
+    module.prometheus.vm_ids,
+    module.loki.vm_ids,
+    module.grafana.vm_ids,
+    module.runner.vm_ids,
+    module.dev-web.vm_ids,
+    module.dev-db.vm_ids,
+    module.backup.vm_ids,
+    module.web.vm_ids,
+    module.proxy.vm_ids
   )
 
   all_vm_ips = concat(
-    /*     module.database.vm_ips,
-    module.backup.vm_ips,
-    module.database-dev.vm_ips,
-    module.server-web-dev.vm_ips,
-    module.ci-cd.vm_ips,
-    module.reverse-proxy.vm_ips,
-    module.server-web.vm_ips,
-    module.dns.vm_ips, */
-    module.bastion.vm_ips,
-    /*     module.grafana.vm_ips,
-    module.loki.vm_ips,
-    module.prometheus.vm_ips */
+    module.bastion.vm_names,
+    module.db.vm_names,
+    module.prometheus.vm_names,
+    module.loki.vm_names,
+    module.grafana.vm_names,
+    module.runner.vm_names,
+    module.dev-web.vm_names,
+    module.dev-db.vm_names,
+    module.backup.vm_names,
+    module.web.vm_names,
+    module.proxy.vm_names
   )
 }
 
@@ -42,8 +40,4 @@ output "vm_ips" {
 output "vm_names" {
   value       = local.all_vm_names
   description = "Nom des VMs déployés"
-}
-
-output "debug_ssh_keys" {
-  value = module.bastion.debug_ci_ssh_keys
 }
